ee662f3aa5985c495417e65f75a0fbe1707c24ec
Justin Ta <justinta1314@yahoo.com>
2026-08-22 20:39:03 -0500

    Initial Commit

---
+++ TenBack/TenBackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenBackApp.swift
+//  TenBack
+//
+//  Created by Justin Ta on 8/22/26.
+//
+
+import SwiftUI
+
+@main
+struct TenBackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
